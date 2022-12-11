8f4fd2a41c227fc7a895c1349809489c8c60c18c
dannab <daniele.cicchinelli@gmail.com>
2022-12-12 00:18:07 +0100

    some bug on gcodemaker

--- app/src/main/java/in/co/gorest/grblcontroller/util/SimpleGcodeMaker.java
+++ app/src/main/java/in/co/gorest/grblcontroller/util/SimpleGcodeMaker.java
@@ -185,17 +185,17 @@ public  class SimpleGcodeMaker {
         return this.ZLoop(s,this.xf,this.yf);
     }
     public String CorneringCut (double tool_radius) {//tr tool radius
-       //,-300,400 ,0.0,-10.0
+
         String s="(cornering cut)\n";
-        s += "G01X"+(this.xf+tool_radius)+" Y"+this.yf+"\n";//start-290.000,400.000
-        s += ("G01X" + (this.xt-tool_radius)  + "\n");//1seg -10.000,400.000
-        s += ("G02X" + this.xt +"Y" + (this.yf-tool_radius) +"I0.000J-"+tool_radius+ "\n");//1arc0.000,390.000
-        s += ("G01Y" + (this.yt+tool_radius)  + "\n");//2seg 0.000,0.000
-        s += ("G02X" + (this.xt-tool_radius) +"Y" + this.yt +"I-"+tool_radius+"J0.0000\n");//2arc  -10.000,-10.000
-        s += ("G01X" + (this.xf+tool_radius)+ "\n");//3seg -290.000,-10.000
-        s += ("G02X" + this.xf +"Y" +(this.yt+tool_radius)  +"I0.000J"+tool_radius+ "\n");//3arc -300.000,0.000
-        s += ("G01Y" + (this.yf-tool_radius) + "\n");//4seg -300.000,390.000
-        s += ("G02X" +(this.xf+tool_radius) +"Y" + this.yf +"I"+tool_radius+"J0.0000\n");//4arc -290.000,400.000
+        s += "G01X"+(this.xf+tool_radius)+" Y"+this.yf+"\n";//start
+        s += ("G01X" + (this.xt-tool_radius)  + "\n");//1seg
+        s += ("G02X" + this.xt +"Y" + (this.yf-tool_radius) +"I0.000J-"+tool_radius+ "\n");//1arc
+        s += ("G01Y" + (this.yt+tool_radius)  + "\n");//2seg
+        s += ("G02X" + (this.xt-tool_radius) +"Y" + this.yt +"I-"+tool_radius+"J0.0000\n");//2arc
+        s += ("G01X" + (this.xf+tool_radius)+ "\n");//3seg
+        s += ("G02X" + this.xf +"Y" +(this.yt+tool_radius)  +"I0.000J"+tool_radius+ "\n");//3arc
+        s += ("G01Y" + (this.yf-tool_radius) + "\n");//4seg
+        s += ("G02X" +(this.xf+tool_radius) +"Y" + this.yf +"I"+tool_radius+"J0.0000\n");//4arc
 
         return this.ZLoop(s, this.xf+tool_radius,this.yf);
     }
